9613602f6fcfa682e578e12dece053678e2f6c93
AntoinePollet <pollet.antoine.alexis@gmail.com>
2019-01-13 22:15:24 +0100

    New stuff

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,57 @@
 
 <template>
     <div id="app">
-        <!--
-        Even when routes use the same component, treat them
-        as distinct and create the component again.
-        -->
+
         <div class="title">HELLO</div>
+        <Home/>
+        <div class="nem">
+            {{derp}}
+        </div>
+        <button v-on:click="changeNem()">change </button>
+        <div v-for="album in albums" v-bind:key="album.id"> 
+            <h1>
+                {{album.title}}
+            </h1>
+            <img :src="album.url"/>
+        </div>
     </div>
 </template>
 
 <script>
-
+    import Home from './Home.vue'
+    export default {
+        data() {
+            return {
+                derp: 'je suis un homme',
+                albums: []
+            }
+        },
+        methods: {
+            changeNem() {
+                this.derp = 'antoine'
+                console.log("pass")
+            },
+            getData() {
+                fetch("https://jsonplaceholder.typicode.com/photos").then(data => 
+                    data.json()
+                ).then((json) => {
+                    console.log(json)
+                    this.albums=json.slice(0,20)
+                })
+            }
+        },
+        created() {
+            this.getData()
+        },
+        components: {Home}
+    }
 </script>
 
-<style scoped>
+<style scoped> 
     .title {
         color:rebeccapurple;
+    }
+    .nem {
+        font-weight: bold;
     }
 </style>
---
+++ src/Home.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+
+    <div class="title">KEk</div>
+    </div>
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+    .title {
+        color:rgb(51, 116, 153);
+    }
+</style>
