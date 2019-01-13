6a93c0477f99a1ad6ac1906f418060d32bd850da
Romain Bonhomme <romain.bonhomme89@gmail.com>
2019-01-13 14:58:54 -0500

    comp added

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+
+<template>
+    <div id="app">
+        <!--
+        Even when routes use the same component, treat them
+        as distinct and create the component again.
+        -->
+        <div class="title">HELLO</div>
+    </div>
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+    .title {
+        color:rebeccapurple;
+    }
+</style>
